455c2ca6c963988b4da2a7f58ecbcd1abb4e549a
morpheus <mariageorgepapas@gmail.com>
2019-11-04 10:36:40 -0600

    New template "add"/removed bootrun addResources(it doesn't exist anymore)

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,7 +1,10 @@
 package org.launchcode.cheesemvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("cheese")
@@ -9,7 +12,22 @@ public class CheeseController {
 
     // Request path: /cheese
     @RequestMapping(value="")
-    public String index() {
+    public String index(Model model) {
+
+        ArrayList<String> cheeses = new ArrayList<>();
+        cheeses.add("cheddar");
+        cheeses.add("parmesan");
+        cheeses.add("munster");
+
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "My Cheeses");
         return "cheese/index";
+    }
+
+    @RequestMapping(value = "add")
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+
     }
 }
